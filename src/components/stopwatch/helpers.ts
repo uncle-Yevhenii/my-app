9ddfeff05c443f135a6ebe224d92390b8b83265d
uncle-Yevhenii <yevhenii.katsimon@gmail.com>
2024-08-01 22:35:02 +0300

    stopwatch 0.1

---
+++ src/components/stopwatch/helpers.ts
@@ -0,0 +1,31 @@
+import React, { Dispatch } from 'react'
+
+export const formatTime = (elapsedTime: number): string => {
+  const h = String(Math.floor(elapsedTime / (1000 * 60 * 60))).padStart(2, '0')
+  const m = String(Math.floor((elapsedTime / (1000 * 60)) % 60)).padStart(2, '0')
+  const s = String(Math.floor((elapsedTime / 1000) % 60)).padStart(2, '0')
+  const ms = String(Math.floor((elapsedTime % 1000) / 10)).padStart(2, '0')
+
+  return `${h}:${m}:${s}:${ms}`
+}
+
+export const start = (
+  setIsRunning: Dispatch<React.SetStateAction<boolean>>,
+  startTimeRef: React.MutableRefObject<number>,
+  elapsedTime: number
+): void => {
+  setIsRunning(true)
+  startTimeRef.current = Date.now() - elapsedTime
+}
+
+export const pause = (setIsRunning: Dispatch<React.SetStateAction<boolean>>): void => {
+  setIsRunning(false)
+}
+
+export const stop = (
+  setElapsedTime: Dispatch<React.SetStateAction<number>>,
+  setIsRunning: Dispatch<React.SetStateAction<boolean>>
+): void => {
+  setElapsedTime(0)
+  setIsRunning(false)
+}
--- src/components/stopwatch/stopwatch.tsx
+++ src/components/stopwatch/stopwatch.tsx
@@ -1,5 +1,38 @@
-import style from './style.module.css'
+import { useState, useEffect, useRef } from 'react'
+import { formatTime, pause, start, stop } from './helpers'
 
-export default function Stopwatch() {
-  return <div></div>
+export function Stopwatch() {
+  const [isRunning, setIsRunning] = useState<boolean>(false)
+  const [elapsedTime, setElapsedTime] = useState<number>(0)
+  const intervalIdRef = useRef<number>(0)
+  const startTimeRef = useRef<number>(0)
+
+  useEffect(() => {
+    if (isRunning) {
+      intervalIdRef.current = setInterval(() => {
+        setElapsedTime(Date.now() - startTimeRef.current)
+      }, 10)
+    }
+
+    return () => {
+      clearInterval(intervalIdRef.current)
+    }
+  }, [isRunning])
+
+  return (
+    <div className="stopwatch">
+      <div className="display">{formatTime(elapsedTime)}</div>
+      <div className="controls">
+        <button onClick={() => start(setIsRunning, startTimeRef, elapsedTime)} className="start-button">
+          Start
+        </button>
+        <button onClick={() => pause(setIsRunning)} className="stop-button">
+          Pause
+        </button>
+        <button onClick={() => stop(setElapsedTime, setIsRunning)} className="reset-button">
+          Stop
+        </button>
+      </div>
+    </div>
+  )
 }
