b2f1a42401b4f56919ddbe3f7090caa04c6aa9a3
uncle-Yevhenii <yevhenii.katsimon@gmail.com>
2024-08-01 15:21:10 +0300

    based timer style

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,7 +4,17 @@ export default function App() {
   return (
     <>
       <section className={style.section}>
-        <div className={style.container}>app</div>
+        <div className={style.display}>DISPLAY</div>
+        <div className={style.btn_navigation}>
+          <div className={style.btn_wrapper}>
+            <button type="button" className={style.btn}>
+              PLAY
+            </button>
+            <button type="button" className={style.btn}>
+              STOP
+            </button>
+          </div>
+        </div>
       </section>
     </>
   )
