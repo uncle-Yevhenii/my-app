33c07d5864e450a4bd46b57bd1040172ecc5db27
uncle-Yevhenii <yevhenii.katsimon@gmail.com>
2024-08-04 13:39:12 +0300

    install package and connect

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App.tsx'
 
+import { Provider } from 'react-redux'
+import { store } from './redux.store/store.ts'
+
 import './styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 )
