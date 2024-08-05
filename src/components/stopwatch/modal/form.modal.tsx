5ee4f70940b0f216802dd7161b94e1bdafffc4b2
uncle-Yevhenii <yevhenii.katsimon@gmail.com>
2024-08-05 21:28:17 +0300

    styling modal

--- src/components/stopwatch/modal/form.modal.tsx
+++ src/components/stopwatch/modal/form.modal.tsx
@@ -2,6 +2,8 @@ import { Field, Form, Formik } from 'formik'
 import { useAppDispatch, useAppSelector } from '@redux/hook'
 import { start, stop, toggleModal } from '@redux/stopwatch/slice.stopwatch'
 
+import style from './style.module.css'
+
 interface Values {
   page: number
 }
@@ -28,10 +30,20 @@ export function FormModal() {
         }
       }}
     >
-      <Form>
-        <label htmlFor="page">Page</label>
-        <Field id="page" name="page" type="number" placeholder="Page" />
-        <button type="submit">Submit</button>
+      <Form className={style.modal__form}>
+        <label htmlFor="page" className={style.modal__label}>
+          {state.buttonText === 'play' ? 'Start page' : 'Finish page'}
+        </label>
+        <Field
+          className={style.modal__input}
+          id="page"
+          name="page"
+          type="number"
+          placeholder="Page"
+        />
+        <button type="submit" className={style.modal__button}>
+          Submit
+        </button>
       </Form>
     </Formik>
   )
--- src/components/stopwatch/modal/root.modal.tsx
+++ src/components/stopwatch/modal/root.modal.tsx
@@ -3,6 +3,9 @@ import { useAppDispatch, useAppSelector } from '@redux/hook'
 import { toggleModal } from '@redux/stopwatch/slice.stopwatch'
 import { FormModal } from './form.modal'
 
+import style from './style.module.css'
+import { CloseIcon } from '@/components/images'
+
 Modal.setAppElement('#root')
 
 export function RootModal() {
@@ -16,17 +19,18 @@ export function RootModal() {
           position: 'fixed',
           top: 0,
           left: 0,
-          right: -100,
+          right: 0,
           bottom: 0,
           backgroundColor: 'transparent',
         },
         content: {
-          maxWidth: '500px',
           position: 'absolute',
-          top: '40px',
-          left: '40px',
-          right: '40px',
-          bottom: '40px',
+          top: '45%',
+          left: '50%',
+          right: 'auto',
+          bottom: 'auto',
+          marginRight: '-50%',
+          transform: 'translate(-50%, -50%)',
           border: '1px solid #b9b3ac',
           background: '#cccccc',
           overflow: 'auto',
@@ -34,14 +38,19 @@ export function RootModal() {
           borderRadius: '0.375rem',
           outline: 'none',
           padding: '20px',
+          boxShadow: '0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1)',
         },
       }}
       isOpen={modal.open}
       onRequestClose={() => dispatch(toggleModal(!modal.open))}
       contentLabel="Example Modal"
     >
-      <button onClick={() => dispatch(toggleModal(!modal.open))}>close</button>
-      <div>I am a modal</div>
+      <button
+        onClick={() => dispatch(toggleModal(!modal.open))}
+        className={style.modal__btnClose}
+      >
+        <CloseIcon size={10} />
+      </button>
       <FormModal />
     </Modal>
   )
